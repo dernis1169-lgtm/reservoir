edd99807384ae7ab5578ea3052ad0e07fd8f85d9
dernis1169-lgtm <dernis1169@gmail.com>
2026-09-19 15:10:10 +0300

    Refactor WebSocket server to use Socket.IO

--- main.go
+++ main.go
@@ -1,70 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"sync"
 
-	"github.com/gorilla/websocket"
+	socketio "github.com/googollee/go-socket.io"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-type Server struct {
-	clients map[*websocket.Conn]bool
-	mu      sync.Mutex
-}
-
-func (s *Server) handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer ws.Close()
-
-	s.mu.Lock()
-	s.clients[ws] = true
-	s.mu.Unlock()
-
-	for {
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			s.mu.Lock()
-			delete(s.clients, ws)
-			s.mu.Unlock()
-			break
-		}
-		s.broadcast(msg)
-	}
-}
-
-func (s *Server) broadcast(msg []byte) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	for client := range s.clients {
-		err := client.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			client.Close()
-			delete(s.clients, client)
-		}
-	}
-}
-
 func main() {
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "joinRoom", func(s socketio.Conn, roomId string) {
+		s.Join(roomId)
+		fmt.Printf("User %s joined room %s\n", s.ID(), roomId)
+	})
+
+	server.OnEvent("/", "draw", func(s socketio.Conn, data interface{}) {
+		// Получаем данные и рассылаем их всем в комнате
+		// В Android приложении мы отправим roomId в объекте данных
+		if m, ok := data.(map[string]interface{}); ok {
+			if roomId, ok := m["roomId"].(string); ok {
+				s.To(roomId).Emit("draw", data)
+			}
+		}
+	})
+
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("error:", e)
+	})
+
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "10000"
+		port = "8080"
 	}
 
-	server := &Server{
-		clients: make(map[*websocket.Conn]bool),
-	}
-
-	http.HandleFunc("/ws", server.handleConnections)
-	log.Printf("Server started on :%s", port)
+	http.Handle("/socket.io/", server)
+	log.Printf("Server started on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
